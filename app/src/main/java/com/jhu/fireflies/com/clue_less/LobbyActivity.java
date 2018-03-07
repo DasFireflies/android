4f9d17f7bec78b3697a1e8b5b46508c19cf2c504
svettom <shawnvettom@gmail.com>
2018-03-06 21:19:28 -0500

    First Upload
    starting point

---
+++ app/src/main/java/com/jhu/fireflies/com/clue_less/LobbyActivity.java
@@ -0,0 +1,13 @@
+package com.jhu.fireflies.com.clue_less;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LobbyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lobby_layout);
+    }
+}
---
+++ app/src/main/java/com/jhu/fireflies/com/clue_less/MainMenuActivity.java
@@ -0,0 +1,13 @@
+package com.jhu.fireflies.com.clue_less;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_menu_layout);
+    }
+}
